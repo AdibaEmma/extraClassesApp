0af3a1cebf5e06d8413f54f1b741601aeaa7e1fc
Emmanuel Aweperi Adiba <eabaagah@gmail.com>
2020-02-28 05:10:18 +0000

    added frontend scaffolding

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin/{id}', 'AdminController@show');
+Route::get('/user/{id}', 'UserController@show');
+Route::get('/tutor/{id}', 'TutorController@show');
